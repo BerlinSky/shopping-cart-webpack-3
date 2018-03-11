8f12c406530266fefea801937c7e009dd9242bdf
Berlin Sky <pedroyoung8989@gmail.com>
2018-03-10 22:37:18 -0800

    Add cart sumit button component

---
+++ app/js/components/CartSubmitButton.vue
@@ -0,0 +1,21 @@
+<template>
+    <form class="l-grid__container cart___buttonContainer js-dataForm" method="post" action="payment.html">
+    <input type="hidden" name="sizeSelection" value="" class="js-sizeSelectionHidden">
+    <button type="submit" class="l-cartActionBlock cartActionBlock actionButton cart___navButton">{{ caption }}</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'CartSubmitButton',
+  data () {
+    return {
+      caption: 'Go to payment'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
